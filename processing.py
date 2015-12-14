4621f243b0e4e51f16851dea8f29df6bf34bd3f1
John Layt <jlayt@kde.org>
2015-12-14 16:41:09 +0000

    Add Processing routine to dissolve features

--- processing.py
+++ processing.py
@@ -10,6 +10,7 @@
         copyright            : (C) 2015 by L - P: Heritage LLP
         copyright            : (C) 2015 by John Layt
         copyright            : (C) 2013 by Piotr Pociask
+        copyright            : (C) 2013 by Victor Olaya
         email                : john@layt.net
  ***************************************************************************/
 
@@ -27,6 +28,7 @@ from qgis.core import QgsFeature, QgsGeometry
 from shapely.ops import polygonize, unary_union
 from shapely.geometry import MultiLineString
 
+# Adapted from QGIS Processing plugin Polygonize
 def polygonizeFeatures(features, fields=None):
     allLinesList = []
     for inFeat in features:
@@ -38,9 +40,30 @@ def polygonizeFeatures(features, fields=None):
     allLines = MultiLineString(allLinesList)
     allLines = unary_union(allLines)
     polygons = list(polygonize([allLines]))
-    outFeat = QgsFeature(fields)
     outList = []
     for polygon in polygons:
+        outFeat = QgsFeature(fields)
         outFeat.setGeometry(QgsGeometry.fromWkt(polygon.wkt))
         outList.append(outFeat)
     return outList
+
+# Adapted from QGIS Processing plugin Dissolve
+def dissolveFeatures(features, fields=None, attributes=None):
+    outFeat = QgsFeature(fields)
+    first = True
+    for inFeat in features:
+        if first:
+            tmpInGeom = QgsGeometry(inFeat.geometry())
+            outFeat.setGeometry(tmpInGeom)
+            first = False
+        else:
+            tmpInGeom = QgsGeometry(inFeat.geometry())
+            tmpOutGeom = QgsGeometry(outFeat.geometry())
+            try:
+                tmpOutGeom = QgsGeometry(tmpOutGeom.combine(tmpInGeom))
+                outFeat.setGeometry(tmpOutGeom)
+            except:
+                pass
+    if attributes is not None:
+        outFeat.setAttributes(attributes)
+    return outFeat
